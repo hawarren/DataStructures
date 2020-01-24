a4e99549fd94996c719514376311792a7b278fd6
Hanif Warren <Hanif.warren@gmail.com>
2020-01-23 21:25:41 -0500

    made priority queue with dynamic array. Removed circular queue code. Code builds and works as expected.

--- DataStructures/HWPQueue.cs
+++ DataStructures/HWPQueue.cs
@@ -37,6 +37,15 @@ namespace DataStructures
                 return false;
             else return true;
         }
+        private void increaseSize()
+        {
+            int?[] newArray = new int?[(int)(internalSize * 1.5)];
+            for (int i = 0; i < internalSize; i++)
+            {
+                newArray[i] = arrayQueue[i];
+            }
+            arrayQueue = newArray;
+        }
         private bool isFull()
         {
             if (internalSize == arrayQueue.Length)
@@ -45,48 +54,30 @@ namespace DataStructures
         }
         public void enqueue(int data)
         {
-            if (!isFull())
+            if (isFull())
+                increaseSize();
+
+
+            int newSpot = tail + 1; //starting point of newspot
+            if (isEmpty())
             {
-                int i;
-                int newSpot = tail+1; //starting point of newspot
-                if (isEmpty())
-                {
-                    arrayQueue[head] = data; //simplify this, relic of normal queue structure
-                    internalSize++;
-                    return; //adding first item to empty queue, tail stays 0 (and tail = head)
-                }
-                //if not empty,instead of inserting at end, first compare to the tail, then move each higher item up
-                //until insertion point is reached.
-                //when we reach the head, move it over and then assign it to the spot
-                while (data < arrayQueue[newSpot-1])//go from tail to the head, may wrap around queue
-                {
-                    arrayQueue[(newSpot ) % arrayQueue.Length] = arrayQueue[newSpot-1]; //move higher value over
-                    if (head == newSpot)
-                        break; //we're at head, don't move over past the head, leave the loop
-                    if (newSpot > 0)
-                        --newSpot; //decrement so we can try the next cell compare
-                    else
-                        newSpot = arrayQueue.Length - 1;//if we reach the 0 index, wrap around Queue to last index in array;
-
-                }
-                if(head==newSpot)
-                {
-                    arrayQueue[newSpot+1] = data; //we found our insertion point (possibly at head), add data here
-
-
-                }
-                else
-                {
-
-                arrayQueue[newSpot] = data; //we found our insertion point (possibly at head), add data here
-                }
-                tail = (tail + 1) % arrayQueue.Length; //move right a spot, wrap around to front of array if at end
-                ++internalSize; //increment the count of items in the queue
+                arrayQueue[head] = data; //simplify this, relic of normal queue structure
+                internalSize++;
+                return; //adding first item to empty queue, tail stays 0 (and tail = head)
             }
-            else
+            //if not empty,instead of inserting at end, first compare to the tail, then move each higher item up
+            //until insertion point is reached.
+            //when we reach the head, move it over and then assign it to the spot
+            while (data < arrayQueue[newSpot - 1])//go from tail to the head, may wrap around queue
             {
-                Console.WriteLine("Sorry the queue is full");
+                arrayQueue[newSpot] = arrayQueue[newSpot-1]; //move higher value over 1 cell
+                newSpot--; //decrement so we can try the next cell compare
+                if (newSpot == head)
+                    break;
             }
+            arrayQueue[newSpot] = data; //we found our insertion point (possibly at head), add data here
+            tail = tail + 1; //move right a spot, wrap around to front of array if at end
+            ++internalSize; //increment the count of items in the queue
         }
         public int dequeue()
         {
@@ -96,7 +87,7 @@ namespace DataStructures
                 arrayQueue[head] = null;
                 internalSize--;
 
-                head = (head + 1) % arrayQueue.Length; //move head to new first in line
+                head = head + 1; //move head to new first in line
                 return (int)removedItem;
             }
             else throw new System.IndexOutOfRangeException("value is higher than this list length");
